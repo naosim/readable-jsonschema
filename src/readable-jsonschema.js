0aca95ced1315fedfcb3fc184f089f34d1a8d0df
naosim <fujitanao@fujitaMacBook-Pro.local>
2018-02-27 07:44:01 +0900

    出力順序調整

--- src/readable-jsonschema.js
+++ src/readable-jsonschema.js
@@ -34,15 +34,28 @@
     obj.type = type
     return obj
   }
+
+  function formatObject(obj) {
+    var result = {}
+    var priority = ['$schema', 'type', 'title', 'description']
+      .reduce((memo, k)=>{ memo[k] = true;return memo; }, {})
+    Object.keys(priority).forEach(k => {
+      if(obj[k]) {
+        result[k] = obj[k]
+      }
+    })
+    Object.keys(obj).filter(k => !priority[k]).forEach(k => result[k] = obj[k])
+    return result
+  }
   
-  var string = (v1, v2) => putType('string', parseArgs2(v1, v2))
-  var stringOption = (v1, v2) => putOption(string(v1, v2))
+  var string = (v1, v2) => formatObject(putType('string', parseArgs2(v1, v2)))
+  var stringOption = (v1, v2) => formatObject(putOption(string(v1, v2)))
   
-  var number = (v1, v2) => putType('number', parseArgs2(v1, v2))
-  var numberOption = (v1, v2) => putOption(number(v1, v2))
+  var number = (v1, v2) => formatObject(putType('number', parseArgs2(v1, v2)))
+  var numberOption = (v1, v2) => formatObject(putOption(number(v1, v2)))
   
-  var boolean = (v1, v2) => putType('boolean', parseArgs2(v1, v2))
-  var booleanOption = (v1, v2) => putOption(boolean(v1, v2))
+  var boolean = (v1, v2) => formatObject(putType('boolean', parseArgs2(v1, v2)))
+  var booleanOption = (v1, v2) => formatObject(putOption(boolean(v1, v2)))
   
   function object(v1, v2, v3) {
     var properties = v1
@@ -51,18 +64,18 @@
     option.properties = properties
     option.type = 'object'
     option.required = required
-    return option;
+    return formatObject(option)
   }
-  var objectOption = (v1, v2, v3) => putOption(object(v1, v2, v3))
+  var objectOption = (v1, v2, v3) => formatObject(putOption(object(v1, v2, v3)))
   
   function array(v1, v2, v3) {
     var items = v1
     option = parseArgs2(v2, v3)
     option.items = items
     option.type = 'array'
-    return option;
+    return formatObject(option)
   }
-  var arrayOption = (v1, v2, v3) => putOption(array(v1, v2, v3))
+  var arrayOption = (v1, v2, v3) => formatObject(putOption(array(v1, v2, v3)))
   
   function deleteIsRequired(obj) {
     if(Array.isArray(obj)) {
